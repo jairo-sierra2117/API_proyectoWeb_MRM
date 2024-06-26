972dd4e095987f6a31e1df3a592667869d63134a
jairo-sierra2117 <jairoenriquesb@ufps.edu.co>
2024-06-25 21:22:19 -0500

    ya funciona ventas , falta agg los mensajes de la operacion.

--- js/Ventas.js
+++ js/Ventas.js
@@ -1,32 +1,4 @@
 $(document).ready(function () {
-    // Función para mostrar alerta
-    function mostrarAlerta(productosConBajaCantidad) {
-        let alertaHtml = `
-            <div class="modal fade" id="alertModal" tabindex="-1" role="dialog" aria-labelledby="alertModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="alertModalLabel">Productos con baja cantidad</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            <ul>
-                                ${productosConBajaCantidad.map(producto => `<li>${producto.descripcion} (Cantidad: ${producto.stock})</li>`).join('')}
-                            </ul>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-primary" data-dismiss="modal">Aceptar</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        `;
-        $('body').append(alertaHtml);
-        $('#alertModal').modal('show');
-    }
-
     // Función para cargar datos del inventario desde la API
     function cargarDatosInventario() {
         fetch('http://localhost:8080/api/productos')
@@ -60,8 +32,7 @@ $(document).ready(function () {
                             <td>${producto.marcaId}</td>
                             <td>${producto.precioCosto}</td>
                             <td>${producto.precioVenta}</td>
-                            <td><button class="btn btn-warning btn-sm editButton">Edit</button></td>
-                            <td><button class="btn btn-custom" onclick="addToSales('${producto.codigo}', '${producto.descripcion}', ${producto.precioVenta})">+</button></td>
+                            <td><button class="btn btn-custom" onclick="addToSales('${producto.id}','${producto.codigo}', '${producto.descripcion}', ${producto.precioVenta})">+</button></td>
                         </tr>
                     `;
                     tbody.append(row);
@@ -86,186 +57,20 @@ $(document).ready(function () {
         });
     });
 
-    // Editar elementos
-    $('#inventoryTable').on('click', '.editButton', function () {
-        let id = $(this).closest('tr').find('td:eq(1)').text(); // Obtener el ID del producto
-        cargarDatosEdicion(id); // Cargar datos del producto para edición
-    });
-
-    // Guardar cambios en la edición
-    $('#saveChangesButton').click(function () {
-        let id = $('#editId').val();
-        let formData = {
-            descripcion: $('#editName').val(),
-            codigo: $('#editCode').val(),
-            categoriaId: $('#editCategory').val(),
-            stock: $('#editQuantity').val(),
-            marcaId: $('#editSupplier').val(),
-            precioCosto: $('#editCost').val(),
-            precioVenta: $('#editSaleCost').val()
-        };
-
-        // Realizar solicitud PUT al backend para actualizar el producto
-        editarProducto(id, formData);
-    });
-
-    // Mostrar modal de creación al hacer clic en Agregar
-    $('#btnAgregar').click(function () {
-        $('#createModal').modal('show');
-    });
-
-    // Enviar formulario de creación al backend
-    $('#createProductButton').click(function () {
-        let formData = {
-            descripcion: $('#nombre').val(),
-            codigo: $('#codigo').val(),
-            categoriaId: parseInt($('#categoriaId').val()),
-            marcaId: parseInt($('#marcaId').val()),
-            stock: parseInt($('#cantidad').val()),
-            precioCosto: parseFloat($('#precioCosto').val()),
-            precioVenta: parseFloat($('#precioVenta').val())
-        };
-
-        // Realizar la solicitud POST al backend para crear un nuevo producto
-        crearProducto(formData);
-    });
-
-    // Función para cargar datos de un producto para edición
-    function cargarDatosEdicion(id) {
-        fetch(`http://localhost:8080/api/productos/${id}`)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Error al obtener datos del producto');
-                }
-                return response.json();
-            })
-            .then(data => {
-                $('#editId').val(data.id);
-                $('#editName').val(data.descripcion);
-                $('#editCode').val(data.codigo);
-                $('#editCategory').val(data.categoriaId);
-                $('#editQuantity').val(data.stock);
-                $('#editSupplier').val(data.marcaId);
-                $('#editCost').val(data.precioCosto);
-                $('#editSaleCost').val(data.precioVenta);
-                $('#editModal').modal('show');
-            })
-            .catch(error => {
-                console.error('Error al cargar datos para edición:', error);
-            });
-    }
-
-    // Función para editar un producto por ID en el backend
-    function editarProducto(id, formData) {
-        fetch(`http://localhost:8080/api/productos/${id}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formData),
-        })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Error al editar el producto');
-                }
-                return response.json();
-            })
-            .then(data => {
-                console.log('Producto editado exitosamente:', data);
-                // Actualizar la fila en la tabla con los nuevos datos
-                $('#inventoryTable tbody tr').each(function () {
-                    if ($(this).find('td:eq(1)').text() === id) {
-                        $(this).find('td:eq(3)').text(formData.descripcion);
-                        $(this).find('td:eq(4)').text(formData.codigo);
-                        $(this).find('td:eq(5)').text(formData.categoriaId);
-                        $(this).find('td:eq(6)').text(formData.stock);
-                        $(this).find('td:eq(7)').text(formData.marcaId);
-                        $(this).find('td:eq(8)').text(formData.precioCosto);
-                        $(this).find('td:eq(9)').text(formData.precioVenta);
-                        return false; // Salir del bucle each
-                    }
-                });
-                $('#editModal').modal('hide');
-            })
-            .catch(error => {
-                console.error('Error al editar el producto:', error);
-            });
-    }
-
-    // Función para crear un nuevo producto en el backend
-    function crearProducto(formData) {
-        fetch('http://localhost:8080/api/productos/create', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formData),
-        })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Error al crear el producto');
-                }
-                return response.json();
-            })
-            .then(data => {
-                console.log('Producto creado exitosamente:', data);
-                // Agregar la nueva fila a la tabla
-                const newRow = `
-                <tr>
-                    <td><input type="checkbox"></td>
-                    <td>${data.id}</td>
-                    <td>${data.index}</td>
-                    <td>${data.descripcion}</td>
-                    <td>${data.codigo}</td>
-                    <td>${data.categoriaId}</td>
-                    <td>${data.stock}</td>
-                    <td>${data.marcaId}</td>
-                    <td>${data.precioCosto}</td>
-                    <td>${data.precioVenta}</td>
-                    <td><button class="btn btn-warning btn-sm editButton">Edit</button></td>
-                    <td><button class="btn btn-custom" onclick="addToSales('${data.codigo}', '${data.descripcion}', ${data.precioVenta})">+</button></td>
-                </tr>
-            `;
-                $('#inventoryTable tbody').append(newRow);
-                $('#createModal').modal('hide');
-            })
-            .catch(error => {
-                console.error('Error al crear el producto:', error);
-            });
-    }
-
-    // Función para eliminar un producto por ID en el backend
-    function eliminarProducto(id) {
-        fetch(`http://localhost:8080/api/productos/${id}`, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Error al eliminar el producto');
-                }
-                console.log('Producto eliminado exitosamente');
-            })
-            .catch(error => {
-                console.error('Error al eliminar el producto:', error);
-            });
-    }
-
     // Llamar a la función para cargar los datos del inventario al cargar la página
     cargarDatosInventario();
 
     // Función para añadir productos a la tabla de ventas
-    window.addToSales = function (codigo, descripcion, precio) {
+    window.addToSales = function (id, codigo, descripcion, precio) {
         const tbodySales = $('#salesTable tbody');
         const newRow = `
             <tr>
+                <td>${id}</td>
                 <td>${codigo}</td>
                 <td>${descripcion}</td>
                 <td>${precio}</td>
                 <td><input type="number" min="1" value="1" class="cantidad-input"></td>
-                <td><button class="btn btn-danger btn-sm cancelButton">Cancelar</button></td>
+                
                 <td><button class="btn btn-custom removeButton">X</button></td>
             </tr>
         `;
@@ -277,7 +82,7 @@ $(document).ready(function () {
     function actualizarTotalAPagar() {
         let total = 0;
         $('#salesTable tbody tr').each(function () {
-            const precio = parseFloat($(this).find('td:eq(2)').text());
+            const precio = parseFloat($(this).find('td:eq(3)').text());
             const cantidad = parseInt($(this).find('.cantidad-input').val());
             total += precio * cantidad;
         });
@@ -300,39 +105,40 @@ $(document).ready(function () {
         $('#salesTable tbody').empty();
         actualizarTotalAPagar();
     });
-
-    // Evento para completar la venta y actualizar el inventario
+    // Función para procesar la venta y enviar los datos al endpoint
     $('#completeSaleButton').click(function () {
-        const salesData = [];
-        $('#salesTable tbody tr').each(function () {
-            const codigo = $(this).find('td:eq(0)').text();
-            const cantidad = parseInt($(this).find('.cantidad-input').val());
-            salesData.push({ codigo, cantidad });
-        });
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
 
-        fetch('http://localhost:8080/api/productos/actualizar-inventario', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(salesData),
-        })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Error al actualizar el inventario');
-                }
-                return response.json();
-            })
-            .then(data => {
-                console.log('Inventario actualizado exitosamente:', data);
-                // Recargar los datos del inventario para reflejar los cambios
-                cargarDatosInventario();
-                // Limpiar la tabla de ventas
-                $('#salesTable tbody').empty();
-                actualizarTotalAPagar();
-            })
-            .catch(error => {
-                console.error('Error al actualizar el inventario:', error);
+        const productosVenta = [];
+        $('#salesTable tbody tr').each(function () {
+            const productoId = $(this).find('td').eq(0).text(); // Aquí se asume que el productoId está en la primera columna
+            const cantidad = $(this).find('.cantidad-input').val();
+            productosVenta.push({
+                productoId: parseInt(productoId, 10),
+                cantidad: parseInt(cantidad, 10)
             });
-    });
+        });
+        const totalVenta = parseFloat($('#totalPagar').text());
+        const raw = JSON.stringify({
+            fecha: "2024-06-19",
+            tipoVenta: "venta directa",
+            clienteId: 123, // Cambia esto según sea necesario
+            totalVenta: totalVenta,
+            productosVenta: productosVenta
+        });
+        const requestOptions = {
+            method: "POST",
+            headers: myHeaders,
+            body: raw,
+            redirect: "follow"
+        };
+
+        fetch("http://localhost:8080/api/ventas", requestOptions)
+            .then((response) => response.text())
+            .then((result) => console.log(result))
+            .catch((error) => console.error('Error:', error));
+    }); cargarDatosInventario();
+
+
 });
